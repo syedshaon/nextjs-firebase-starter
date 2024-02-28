b37f8715a9f6895d43c85400cec06ef940df0034
syedshaon <syedshaon99@gmail.com>
2024-02-29 00:22:40 +0600

    added google signin

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,34 +1,55 @@
-'use client'
+"use client";
 import signIn from "@/firebase/auth/signIn";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { signInWithGoogle } from "@/firebase/auth/signIn";
 
 function Page(): JSX.Element {
-  const [ email, setEmail ] = useState( '' );
-  const [ password, setPassword ] = useState( '' );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const router = useRouter();
 
-  // Handle form submission
-  const handleForm = async ( event: { preventDefault: () => void } ) => {
+  const handleSignInWithGoogle = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
-
     // Attempt to sign in with provided email and password
-    const { result, error } = await signIn( email, password );
+    const { result, error } = await signInWithGoogle();
 
-    if ( error ) {
+    if (error) {
       // Display and log any sign-in errors
-      console.log( error );
+      console.log(error);
       return;
     }
 
     // Sign in successful
-    console.log( result );
+    console.log(result);
 
     // Redirect to the admin page
-    // Typically you would want to redirect them to a protected page an add a check to see if they are admin or 
+    // Typically you would want to redirect them to a protected page an add a check to see if they are admin or
     // create a new page for admin
-    router.push( "/admin" );
-  }
+    router.push("/admin");
+  };
+
+  // Handle form submission
+  const handleForm = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+
+    // Attempt to sign in with provided email and password
+    const { result, error } = await signIn(email, password);
+
+    if (error) {
+      // Display and log any sign-in errors
+      console.log(error);
+      return;
+    }
+
+    // Sign in successful
+    console.log(result);
+
+    // Redirect to the admin page
+    // Typically you would want to redirect them to a protected page an add a check to see if they are admin or
+    // create a new page for admin
+    router.push("/admin");
+  };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -39,39 +60,25 @@ function Page(): JSX.Element {
             <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
               Email
             </label>
-            <input
-              onChange={( e ) => setEmail( e.target.value )}
-              required
-              type="email"
-              name="email"
-              id="email"
-              placeholder="example@mail.com"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
+            <input onChange={(e) => setEmail(e.target.value)} required type="email" name="email" id="email" placeholder="example@mail.com" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
           </div>
           <div className="mb-6">
             <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
               Password
             </label>
-            <input
-              onChange={( e ) => setPassword( e.target.value )}
-              required
-              type="password"
-              name="password"
-              id="password"
-              placeholder="password"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
+            <input onChange={(e) => setPassword(e.target.value)} required type="password" name="password" id="password" placeholder="password" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
           </div>
           <div className="flex items-center justify-between">
-            <button
-              type="submit"
-              className="w-full bg-blue-500 text-white font-semibold py-2 rounded"
-            >
+            <button type="submit" className="w-full bg-blue-500 text-white font-semibold py-2 rounded">
               Sign In
             </button>
           </div>
         </form>
+        <div className="flex items-center justify-between">
+          <button onClick={handleSignInWithGoogle} type="submit" className="w-full bg-blue-500 text-white font-semibold py-2 rounded">
+            Continue with Google
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/firebase/auth/signIn.ts
+++ src/firebase/auth/signIn.ts
@@ -1,5 +1,5 @@
 import firebase_app from "../config";
-import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { signInWithEmailAndPassword, signInWithPopup, getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Get the authentication instance using the Firebase app
 const auth = getAuth(firebase_app);
@@ -11,6 +11,21 @@ export default async function signIn(email: string, password: string) {
 
   try {
     result = await signInWithEmailAndPassword(auth, email, password); // Sign in with email and password
+  } catch (e) {
+    error = e; // Catch and store any error that occurs during sign-in
+  }
+
+  return { result, error }; // Return the sign-in result and error (if any)
+}
+
+export async function signInWithGoogle() {
+  let result = null, // Variable to store the sign-in result
+    error = null; // Variable to store any error that occurs
+
+  try {
+    // result = await signInWithEmailAndPassword(auth, email, password); // Sign in with email and password
+    const provider = new GoogleAuthProvider();
+    result = await signInWithPopup(auth, provider); // Sign in with google
   } catch (e) {
     error = e; // Catch and store any error that occurs during sign-in
   }
